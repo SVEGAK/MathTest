11e19307906f55de740ec7404dc4b9e4ff08661d
Новиков Роман <roman141aleksandro2007@gmail.com>
2026-09-17 20:27:01 +0300

    Исправлены ошибки в тестах Task

--- Google Test/test.cpp
+++ Google Test/test.cpp
@@ -7,19 +7,23 @@ TEST(StructTask, DefaultConstructor) {
 	ASSERT_NO_THROW(Task());
 }
 TEST(StructTask, InitConstructorDivide) {
-	Task t(1,2,op_divide);
+    Task t;
+    t = t.exact(1, 2, op_divide);
 	EXPECT_EQ(t.answer, 0);
 }
 TEST(StructTask, InitConstructorMultiple) {
-	Task t(2, 5, op_multiplicate);
+	Task t;
+    t = t.exact(2, 5, op_multiplicate);
 	EXPECT_EQ(t.answer, 10);
 }
 TEST(StructTask, InitConstructorADD) {
-	Task t(2, 5, op_add);
+	Task t;
+    t = t.exact(2, 5, op_add);
 	EXPECT_EQ(t.answer, 7);
 }
 TEST(StructTask, InitConstructorSubtract) {
-	Task t(2, 5, op_subtract);
+	Task t;
+    t = t.exact(2, 5, op_subtract);
 	EXPECT_EQ(t.answer, -3);
 }
 TEST(StructTask, InitConstructorRandom) {
